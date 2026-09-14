8ff893566797bcd5c0d1907576a4b35acf3fbd5a
xpertdreamer <yungnomen@gmail.com>
2026-09-14 23:23:33 +0300

    Function to build alphabet from given source

--- lab_1/substitution.c
+++ lab_1/substitution.c
@@ -3,9 +3,10 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 buffer*
-buffer_alloc(const char *str)
+buffer_alloc(const wchar_t *str)
 {
     DEBUG("Call buffer_alloc");
     if (str == NULL) {
@@ -15,15 +16,15 @@ buffer_alloc(const char *str)
     buffer* result;
     result = (buffer*)malloc(sizeof(buffer));
     ALLOC_FAIL(result);
-    size_t len = strlen(str);
+    size_t len = wcslen(str);
     result->cypher = NULL;
-    result->raw = (char*)malloc((len+1)*sizeof(char));
+    result->raw = (wchar_t*)malloc((len+1)*sizeof(wchar_t));
     if (result->raw == NULL) {
         ERROR("Failed to allocate result->raw");
         free(result);
         return NULL;
     }
-    strcpy(result->raw, str);
+    wcscpy(result->raw, str);
     result->size = len;
     result->N = 0;
     return result;
@@ -40,17 +41,23 @@ buffer_free(buffer* ptr)
 }
 
 void
-build_alphabet(buffer* buf, const char* given)
+build_alphabet(buffer* buf, const wchar_t* given)
 {
     DEBUG("Call build_alphabet");
     PTR_RECIEVE_FAIL_VOID(buf, build_alphabet);
     PTR_RECIEVE_FAIL_VOID(given, build_alphabet);
     buf->N = 0;
-    memset(buf->alphabet, 0, ALPHABET_SIZE);
-    for (int i = 0; given[i] != '\0'; ++i)
-        if (have(buf->alphabet, buf->N, LETTER[i]) == true) buf->alphabet[buf->N++] = LETTER[i];
-    for (int i = 0; given[i] != '\0'; ++i)
-        if (have(buf->alphabet, buf->N, DIGIT[i]) == true) buf->alphabet[buf->N++] = DIGIT[i];
-    for (int i = 0; given[i] != '\0'; ++i)
-        if (have(buf->alphabet, buf->N, PUNCTUATION[i]) == true) buf->alphabet[buf->N++] = PUNCTUATION[i];
+    memset(buf->alphabet, 0, sizeof(buf->alphabet));
+    for (int i = 0; given[i] != '\0'; ++i) {
+        wchar_t c = given[i];
+        if (wcsrchr(LETTER, c) != NULL ||
+            wcschr(DIGIT, c) != NULL ||
+            wcschr(PUNCTUATION, c) != NULL) {
+            if (wcschr(buf->alphabet, c) == NULL) {
+                if (buf->N < ALPHABET_SIZE - 1) {
+                    buf->alphabet[buf->N++] = c;
+                }
+            }
+        }
+    }
 }
