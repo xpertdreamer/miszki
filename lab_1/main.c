8a7f5fa3daf96269150d65bc9e8a8f1a6ef1e1de
xpertdreamer <yungnomen@gmail.com>
2026-09-14 21:53:01 +0300

    Opt proccessor

--- lab_1/main.c
+++ lab_1/main.c
@@ -1,3 +1,18 @@
-int main(void) {
+#include <unistd.h>
 
+int main(int argc, char** argv)
+{
+    int opt;
+    // (require args):
+    // a - alphabet ;
+    // m - mode ;
+    // s - shift ;
+    while ((opt = getopt(argc, argv, "a:m:s:"))!= -1) {
+        switch (opt) {
+            case 'a': break;
+            case 'm': break;
+            case 's': break;
+        }
+    }
+    return 0;
 }
