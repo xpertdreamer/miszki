96123f007ed2c67e472a488a2a2a87475c78678c
xpertdreamer <yungnomen@gmail.com>
2026-09-14 23:18:46 +0300

    Replace char with wchar to properly read russian letters

--- lab_1/substitution.h
+++ lab_1/substitution.h
@@ -2,28 +2,30 @@
 #define SUBSTITUTION_H
 
 #include <stddef.h>
+#include <wchar.h>
 
-#define PUNCTUATION ",.!?;:-()[]{}\"'`@#%^&+=/\\|<>~"
-#define DIGIT       "0123456789"
-#define LETTER      "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+#define PUNCTUATION L",.!?;:-()[]{}\"'`@#%^&+=/\\|<>~"
+#define DIGIT       L"0123456789"
+#define LETTER      L"абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
 
-#define ALPHABET_SIZE 105 // russian letters = 2 bytes
+#define ALPHABET_SIZE 150
 
 typedef struct {
     size_t size;
     int N;
-    char* raw;
-    char* cypher;
-    char alphabet[ALPHABET_SIZE];
+    wchar_t* raw;
+    wchar_t* cypher;
+    wchar_t alphabet[ALPHABET_SIZE];
 } buffer;
 
 buffer*
-buffer_alloc(const char* str);
+buffer_alloc(const wchar_t* str);
 
 void
 buffer_free(buffer* ptr);
 
-
+void
+build_alphabet(buffer* buf, const wchar_t* given);
 
 // TODO: encrypt
 // TODO: decrypt
