b519a3165aef19d258146ef388c0117efc5516cc
xpertdreamer <yungnomen@gmail.com>
2026-09-18 16:20:12 +0300

    Inital API for replacement

---
+++ lab_1/replacement.h
@@ -0,0 +1,33 @@
+#ifndef REPLACEMENT_H
+#define REPLACEMENT_H
+
+#include <stddef.h>
+
+typedef struct {
+    wchar_t from;
+    wchar_t to;
+} entry;
+
+typedef struct {
+    entry* entries;
+    size_t count;
+} replace_table;
+
+typedef struct {
+    size_t size;
+    wchar_t* raw;
+    wchar_t* proccessed;
+} replacement_buffer;
+
+replace_table*
+build_table(const wchar_t* alphabet);
+
+void
+save_replace_table(replace_table* ptr, const char* path);
+
+void
+free_workspace(replace_table* table, replacement_buffer* buf);
+/* replace_table* */
+/* build_replace_table(args); */
+
+#endif
