286eb3cd33c5ed8ad5cdb332fdced9662a405826
xpertdreamer <yungnomen@gmail.com>
2026-09-18 21:11:54 +0300

    Two group of flags

--- lab_1/main.c
+++ lab_1/main.c
@@ -22,11 +22,14 @@ int main(int argc, char** argv)
     // m - mode ;
     // s - shift ;
     // g - given source ;
-    bool a_providen = false, m_providen = false, s_providen = false, g_providen = false;
+    // u - upload table ;
+    // d - download table ;
+    bool a_providen = false, m_providen = false, s_providen = false, g_providen = false, u_providen = false, d_providen = false;
     int opt, s;
     mode m;
-    wchar_t  *a, *g, *alphabet;
-    while ((opt = getopt(argc, argv, "g:a:m:s:")) != -1) {
+    wchar_t  *a = NULL, *g = NULL, *alphabet = NULL;
+    const char* d_path = NULL, *u_path = NULL;
+    while ((opt = getopt(argc, argv, "g:a:m:s:u:d:")) != -1) {
         switch (opt) {
             case 'a': {
                 a = char_to_wchar(optarg);
@@ -74,32 +77,64 @@ int main(int argc, char** argv)
                 g_providen = true;
                 break;
             }
+            case 'u': u_path = optarg; u_providen = true; break;
+            case 'd': d_path = optarg; d_providen = true; break;
             default: HELP(argv[0]);
         }
     }
-    if ((a_providen && m_providen && s_providen && g_providen) == false) {
-        ERROR("Some arguments not providen");
+    bool group1 = a_providen && s_providen && m_providen && g_providen && !u_providen && !d_providen;
+    bool group2 = (u_providen || d_providen) && m_providen && g_providen && !a_providen && !s_providen;
+    if (group1 == true) {
+        buffer* buf;
+        buf = buffer_alloc(g);
+        if (buf == NULL) {
+            ERROR("Failed to allocate buffer");
+            FREE_WCHAR(a);
+            FREE_WCHAR(g);
+            FREE_WCHAR(alphabet);
+            return 1;
+        }
+        build_alphabet(buf, alphabet);
+        switch (m) {
+            case encryption: encrypt(buf, s); break;
+            case decryption: decrypt(buf, s); break;
+        }
+        PRINT_WCHAR(buf->cypher);
+        buffer_free(buf);
+    }
+    else if (group2 == true) {
+        replacement_buffer* buf = replace_buf_alloc(g);
+        if (buf == NULL) {
+            ERROR("Failed to allocate buffer");
+            FREE_WCHAR(a);
+            FREE_WCHAR(g);
+            FREE_WCHAR(alphabet);
+            return 1;
+        }
+        replace_table *table = build_table(u_path);
+        if (table == NULL) {
+            ERROR("Failed to allocate buffer");
+            FREE_WCHAR(a);
+            FREE_WCHAR(g);
+            FREE_WCHAR(alphabet);
+            free(buf);
+            return 1;
+        }
+        switch (m) {
+            case encryption: r_encrypt(table, buf); break;
+            case decryption: r_decrypt(table, buf); break;
+        }
+        PRINT_WCHAR(buf->processed);
+        if (d_providen == true) save_replace_table(table, d_path);
+        free_workspace(table, buf);
+    }
+    else {
+        ERROR("Some arguments not providen, or group of flags is incorrect");
         FREE_WCHAR(a);
         FREE_WCHAR(g);
         FREE_WCHAR(alphabet);
         HELP(argv[0]);
     }
-    buffer* buf;
-    buf = buffer_alloc(g);
-    if (buf == NULL) {
-        ERROR("Failed to allocate buffer");
-        FREE_WCHAR(a);
-        FREE_WCHAR(g);
-        FREE_WCHAR(alphabet);
-        return 1;
-    }
-    build_alphabet(buf, alphabet);
-    switch (m) {
-        case encryption: encrypt(buf, s); break;
-        case decryption: decrypt(buf, s); break;
-    }
-    PRINT_WCHAR(buf->cypher);
-    buffer_free(buf);
     FREE_WCHAR(a);
     FREE_WCHAR(g);
     FREE_WCHAR(alphabet);
