8e341d0aaa0704990f28663d3ae34b35b214eb28
xpertdreamer <yungnomen@gmail.com>
2026-09-26 18:22:44 +0300

    Add debug information

--- lab_2/factorization/factor.go
+++ lab_2/factorization/factor.go
@@ -8,6 +8,7 @@ import (
 const trialDivisiomMinimum = 2
 
 func TrialDivision(n uint) ([]uint, error) {
+	util.Debug("Call TrialDivision")
 	defer util.Measure("TrialDivision")()
 	var result []uint
 	if n < trialDivisiomMinimum {
@@ -15,11 +16,13 @@ func TrialDivision(n uint) ([]uint, error) {
 	}
 	for i := uint(trialDivisiomMinimum); i < n; i++ {
 		for n % i == 0 {
+			util.Debug("[TrialDivision] result += %d", i)
 			result = append(result, i)
 			n /= i
 		}
 	}
 	if n >= trialDivisiomMinimum {
+		util.Debug("[TrialDivision] result += %d", n)
 		result = append(result, n)
 	}
 	return result, nil
