c334b6d2f256ccbd502b760669571fe394a92117
xpertdreamer <yungnomen@gmail.com>
2026-09-26 18:18:35 +0300

    Remove magic numbers

--- lab_2/factorization/factor.go
+++ lab_2/factorization/factor.go
@@ -5,19 +5,21 @@ import (
 	"mizski/lab2/util"
 )
 
+const trialDivisiomMinimum = 2
+
 func TrialDivision(n uint) ([]uint, error) {
 	defer util.Measure("TrialDivision")()
 	var result []uint
-	if n < 2 {
+	if n < trialDivisiomMinimum {
 		return nil, errors.New("TrialDivision: given number must be greater than 1")
 	}
-	for i := uint(2); i < n; i++ {
+	for i := uint(trialDivisiomMinimum); i < n; i++ {
 		for n % i == 0 {
 			result = append(result, i)
 			n /= i
 		}
 	}
-	if n > 1 {
+	if n >= trialDivisiomMinimum {
 		result = append(result, n)
 	}
 	return result, nil
